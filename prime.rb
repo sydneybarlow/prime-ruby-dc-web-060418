fb1907f0b3d622abad860e362ad4f15e24561133
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-31 15:55:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'benchmark'
+
+counter = 1
+
+def prime?(x)
+  if x < 0
+    false 
+  elsif n % x == 0
+    false
+  elsif (x/1) && (x / x)
+    counter =+ 1
+    true
+  else
+    false
+  end
+end  
+
+#(x/1) && (x / x)
+#    counter =+ 1
