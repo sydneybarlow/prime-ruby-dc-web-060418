ef725550f510d789f984d067a71ad92aae50bbe0
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-31 16:59:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require 'benchmark'
+
+counter = 1
+
+def prime?(x)
+  if x < 2 
+    false 
+#  elsif n % x == 0
+#    false
+#  elsif (x/1) && (x / x)
+#    counter =+ 1
+#    true
+  else
+     (2..(x -1)).to_a.each do |num|
+      if x % num == 0 
+        return false 
+      end  
+    end 
+    true 
+  end
+end
